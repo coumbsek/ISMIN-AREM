97c2000ffb518db7a67feae813ab9c2ef1b9fcf2
coumbsek <coumbs.ek@gmail.com>
2016-10-09 22:10:37 +0200

    separation des fonctions de reconnaissance de contours en .h et .cpp distincts du main

---
+++ feteScience/imgRec/recContours.h
@@ -0,0 +1,21 @@
+//#ifndef REC_H
+//#define REC_H
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+void drawStuff(int, void*);
+void displayPicture(int, void*);
+void showInputWindow();
+void showCannyWindow();
+void showContourWindow();
+void killContourAndChildren(int contour);
+double realContourArea(int contour);
+int selectedContour(Point clickPoint);
+void onMouse(int event, int x, int y, int, void*);
+void pathfinding();
+
+//#endif
