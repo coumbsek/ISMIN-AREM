d2e218a47b7e672ff41d677eb31144a55ab9e16f
Vyann <yann.bouteiller@hotmail.fr>
2016-10-10 21:32:49 +0200

    modified:   pathfinding.cpp 	modified:   pathfinding.h

--- feteScience/imgRec/pathfinding.cpp
+++ feteScience/imgRec/pathfinding.cpp
@@ -28,8 +28,8 @@ void pathfinding(double offset)
 	B1 = approxConvex[1];
 	B2 = approxConvex[2];
 	
-	Vect vOffset = offsetVector(A1,B1,offset);
-	Vect vAB = points2Vec(A1,B1);
+	Vector2d vOffset = offsetVector(A1,B1,offset);
+	Vector2d vAB = points2Vec(A1,B1);
 	
 	path.clear();
 	
@@ -41,51 +41,52 @@ void pathfinding(double offset)
 	// puis on recherche les points d'intersection de la parallèle à A1B1 décalée de vOffset
 	
 	Point O = vec2Point(B1, vOffset); // à partir du point 0, on cherche ces points
-	Vect vB12 = points2Vec(B1,B2); // on a besoin de croiser vB12 avec vAB, on vérifie qu'ils ne sont pas colinéaires
+	Vector2d vB12 = points2Vec(B1,B2); // on a besoin de croiser vB12 avec vAB, on vérifie qu'ils ne sont pas colinéaires
+	
 	
 	Matrix4d M;
-	M << 1,-1*vAB.x,0,0,1,0,0,-1*vB12.x,0,-1*vAB.y,1,0,0,0,1,-1*vB12.y;
+	M << 1,-1*vAB[0],0,0,1,0,0,-1*vB12[0],0,-1*vAB[1],1,0,0,0,1,-1*vB12[1];
 	cout << "M : " << endl << M << endl;
 	
 	Vector4d C;
 	C << O.x,B1.x,O.y,B1.y;
 	cout << "C : " << endl << C << endl;
 	
-	FullPivLU<Matrix4d> decomposition(M);
+	FullPivLU<Matrix4d> decompositionB(M);
 
-    	Vector4d X = decomposition.solve(C);
-    	cout << "X : " << endl << X << endl;
+    	Vector4d XB = decompositionB.solve(C); // X : (x, t, y, t')
+    	cout << "XB : " << endl << XB << endl;
 }
 
-Point vec2Point(Point depart, Vect vec)
+Point vec2Point(Point depart, Vector2d vec)
 {
-	double lx = (double)depart.x + vec.x;
-	double ly = (double)depart.y + vec.y;
+	double lx = (double)depart.x + vec[0];
+	double ly = (double)depart.y + vec[1];
 	
 	return Point(ceil(lx),ceil(ly)); 
 }
 
-Vect points2Vec(Point A, Point B) // transforme AB en vecteur unitaire
+Vector2d points2Vec(Point A, Point B) // transforme AB en vecteur unitaire
 {
 	double moduleX = (B.x - A.x);
 	double moduleY = (B.y - A.y);
 	double module = sqrt(moduleX*moduleX + moduleY*moduleY);
 	
-	Vect V;
-	V.x = moduleX/module;
-	V.y = moduleY/module;
+	Vector2d V;
+	V[0] = moduleX/module;
+	V[1] = moduleY/module;
 	
 	return V;
 }
 
-Vect offsetVector(Point A, Point B, double offset) // (unitaire) A et B sont les points de l'arrête à couper, leur ordre spatial ne change pas, et le polygone est codé en sens horraire
+Vector2d offsetVector(Point A, Point B, double offset) // (unitaire) A et B sont les points de l'arrête à couper, leur ordre spatial ne change pas, et le polygone est codé en sens horraire
 {
-	Vect edge = points2Vec(A,B);
+	Vector2d edge = points2Vec(A,B);
 	
-	Vect voffset;
+	Vector2d voffset;
 	
-	voffset.x = (edge.y) * offset;
-	voffset.y = (-1 * edge.x) * offset;
+	voffset[0] = (edge[1]) * offset;
+	voffset[1] = (-1 * edge[0]) * offset;
 	
 	return voffset;
 }
